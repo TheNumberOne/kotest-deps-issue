c5582cd869fe25225c30a163201562aa7f990321
Rosetta Roberts <rosettafroberts@gmail.com>
2020-05-16 16:35:32 -0600

    Updated to latest snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
 }
 
 dependencies {
     implementation("org.junit.jupiter:junit-jupiter:5.6.0")
-    implementation("io.kotest:kotest-assertions-core-jvm:4.0.5")
+    implementation("io.kotest:kotest-assertions-core-jvm:4.1.0.291-SNAPSHOT")
     implementation(kotlin("stdlib-jdk8"))
 }
 
